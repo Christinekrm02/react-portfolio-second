91d6f704f6a8e354315d1ada56f563a8827ef17c
Christine Maynard <christinekrm02@gmail.com>
2020-10-31 17:31:35 -0400

    Added NavLinks

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export default class NavigationContainer extends Component {
+  constructor() {
+    super();
+  }
+  adminLinks() {}
+  render() {
+    return (
+      <div>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink to="about-me">
+          About
+        </NavLink>
+        <NavLink to="contact">
+         contact
+        </NavLink>
+        <NavLink to="blog">
+         Blog
+        </NavLink>
+      
+        {false ? <button>Add Blog</button> : null}
+      </div>
+    );
+  }
+}

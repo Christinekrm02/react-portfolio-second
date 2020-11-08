18464e5d97a23ddac7345e2921f42189924ab3b7
Christine Maynard <christinekrm02@gmail.com>
2020-11-08 18:32:40 -0500

    added overlay text and images to background images

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -7,20 +7,22 @@ export default function (props) {
   //background image: thumb_image_url
   //id: id
   //description: description
-  const { id, description, thumb_image_url, logo } = props.item;
+  const { id, description, thumb_image_url, logo_url } = props.item;
   return (
     <div className="portfolio-item-wrapper">
       <div
         className="portfolio-img-background"
-        //inline styles, expects an object
+        //inline styles for img layout, style expects an object
         style={{
           backgroundImage: "url(" + thumb_image_url + " )",
         }}
       />
-
-      <img src={logo} />
-      <div>{description}</div>
-      <Link to={`/portfolio/${id}`}>Link</Link>
+      <div className="img-text-wrapper">
+        <div className="logo-wrapper">
+          <img src={logo_url} />
+        </div>
+        <div className="subtitle">{description}</div>
+      </div>
     </div>
   );
 }

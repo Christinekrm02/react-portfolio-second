ac840bd3e71e0725b23a2bc773e7242b3c533f97
Christine Maynard <christinekrm02@gmail.com>
2020-11-08 19:14:59 -0500

    added event listener to respond to mouse hover over image tiles

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function (props) {
+export default class PortfolioItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      PortfolioItemClass: "",
+    };
+  }
+  //for event listner: upon mouse hover/upon mouse moving from element
+  handleMouseEnter() {
+    this.setState({ PortfolioItemClass: "image-blur" });
+  }
+  handleMouseLeave() {
+    this.setState({ PortfolioItemClass: " " });
+  }
   //Data needed:
   //backgound image, logo, description and id for slug
   //background image: thumb_image_url
   //id: id
   //description: description
-  const { id, description, thumb_image_url, logo_url } = props.item;
-  return (
-    <div className="portfolio-item-wrapper">
+  render() {
+    const { id, description, thumb_image_url, logo_url } = this.props.item;
+    return (
       <div
-        className="portfolio-img-background"
-        //inline styles for img layout, style expects an object
-        style={{
-          backgroundImage: "url(" + thumb_image_url + " )",
-        }}
-      />
-      <div className="img-text-wrapper">
-        <div className="logo-wrapper">
-          <img src={logo_url} />
+        className="portfolio-item-wrapper"
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}>
+       
+        <div
+        
+          className={"portfolio-img-background " + this.state.PortfolioItemClass}
+          //inline styles for img layout, style expects an object
+          style={{
+            backgroundImage: "url(" + thumb_image_url + " )",
+          }}
+        />
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} />
+          </div>
+          <div className="subtitle">{description}</div>
         </div>
-        <div className="subtitle">{description}</div>
       </div>
-    </div>
-  );
+    );
+  }
 }

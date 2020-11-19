c335980449c03c796409f088bd41f8a357fb0306
Christine Maynard <christinekrm02@gmail.com>
2020-11-19 10:53:59 -0500

    working on render props

--- src/components/app.js
+++ src/components/app.js
@@ -11,6 +11,22 @@ import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+    };
+  }
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+    });
+  }
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+    });
+  }
   render() {
     return (
       <div className="container">
@@ -18,7 +34,17 @@ export default class App extends Component {
           <div>
             <NavigationContainer />
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                  />
+                )}
+              />
               <Route exact path="/auth" component={Auth} />
               <Route exact path="/about-me" component={About} />
               <Route exact path="/contact" component={Contact} />

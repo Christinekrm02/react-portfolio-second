a3383eee6ce9d50483a2d8485b136526ca95d9eb
Christine Maynard <christinekrm02@gmail.com>
2020-11-22 18:08:41 -0500

    TODO: add styles to sidebar list items

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSideBarList from "../portfolio/portfolio-sidebar-list";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+    this.state = {
+      portfolioItems: [],
+    };
+  }
+  getPortfolioItems() {
+    axios
+      .get("https://christinem.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items],
+        });
+      })
+      .catch(error => {
+        console.log("error in PortfolioItemsfunction", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
   render() {
     return (
-      <div>
-        <h3>PortfolioManager</h3>
-        <h2>PortfolioManager</h2>
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio form</h1>
+        </div>
+        <div className="right-column">
+          <PortfolioSideBarList data={this.state.portfolioItems} />
+        </div>
       </div>
     );
   }

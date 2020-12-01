0c9616b1de4be76545c7327d30b8f3e80c8cd09b
Christine Maynard <christinekrm02@gmail.com>
2020-12-01 16:28:04 -0500

    fixed ui for icon, need to remove component ids. TODO: Naming reqs

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -8,13 +8,16 @@ const PortfolioSideBarList = props => {
         <div className="portfolio-item-thumb-img">
           <img src={portfolioItem.thumb_image_url} /> 12
         </div>
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        {/* to delete an image/item on the page*/}
-        {/* method created at portfolio-manager and will be applied to elements rendered in sidebar*/}
-        <a onClick={() => props.handleDeleteClick(portfolioItem)}>
-          <FontAwesomeIcon icon="trash" />
-        </a>
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
+          {/* to delete an image/item on the page*/}
+          {/* method created at portfolio-manager and will be applied to elements rendered in sidebar*/}
+          <a
+            className="delete-icon"
+            onClick={() => props.handleDeleteClick(portfolioItem)}>
+            <FontAwesomeIcon icon="trash" />
+          </a>
+        </div>
       </div>
     );
   });

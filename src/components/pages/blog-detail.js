836be3e5482508a998dbf0f45c4ab732d9451c22
Christine Maynard <christinekrm02@gmail.com>
2020-12-14 17:39:16 -0500

    Made api call to get each blog item from api

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentBlogId: this.props.match.params.slug,
+      blogItem: {},
+    };
+  }
+  getBlogItem() {
+    axios
+      .get(
+        `https://christinem.devcamp.space/portfolio/portfolio_blogs/${this.state.currentBlogId}`
+      )
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log("getBlgItemError", error);
+      });
+  }
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
   render() {
+    console.log("currentBlogId", this.state.currentBlogId);
     return (
       <div>
         <h1>Blog detail</h1>

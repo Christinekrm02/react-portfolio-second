f7f139acfbc39a36179e85f390e85c5161d44747
Christine Maynard <christinekrm02@gmail.com>
2020-12-16 09:34:47 -0500

    began working on infinite scroll functionality

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -11,6 +11,17 @@ export default class Blog extends Component {
       blogItems: [],
     };
     this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
+    };
   }
   getBlogItems() {
     axios

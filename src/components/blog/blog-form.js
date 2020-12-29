7ce96f8896c25a7bc2cefe11014599ab07777b0e
Christine Maynard <christinekrm02@gmail.com>
2020-12-28 20:40:22 -0500

    added draft-js to modal. Need to update styles. NEXT: 10F

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import RichTextEditor from "../forms/rich-text-editor";
+
 export default class BlogForm extends Component {
   constructor(props) {
     super(props);
@@ -65,7 +67,9 @@ export default class BlogForm extends Component {
           placeholder="Blog status"
           value={this.state.blog_status}
         />
-
+        <div className="onc-column">
+          <RichTextEditor />
+        </div>
         <button>Save</button>
       </form>
     );

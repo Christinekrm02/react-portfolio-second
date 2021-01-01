730c19b6ae269ec0d8fe9489c4a179f1a24d18f0
Christine Maynard <christinekrm02@gmail.com>
2021-01-01 12:00:31 -0500

    customized toolbar for rich text editor. NEXT: Convert image files into base64 strings

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -21,6 +21,9 @@ export default class RichTextEditor extends Component {
       )
     );
   }
+  uploadFile(file) {
+    console.log(("upload file:", file));
+  }
   render() {
     return (
       <div>
@@ -29,6 +32,21 @@ export default class RichTextEditor extends Component {
           wrapperClassName="demo-wrapper"
           editorClassName="demo-editor"
           onEditorStateChange={this.onEditorStateChange}
+          /* custom styles for toolbar */
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadFile,
+              alt: { present: true, mandatory: false },
+              previewImage: true,
+              inputAccept:
+                "image/gif,image/jpg,image/jpeg, image/png, image/svg",
+            },
+          }}
         />
       </div>
     );

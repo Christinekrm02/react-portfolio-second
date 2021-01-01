57fd756517f0cbae362cec00ee7341c9f819733c
Christine Maynard <christinekrm02@gmail.com>
2021-01-01 11:37:12 -0500

    next: adding more styles to blog form

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -23,6 +23,8 @@ export default class BlogForm extends Component {
     this.compopnentConfig = this.compopnentConfig.bind(this);
     this.djsConfig = this.djsConfig.bind(this);
     this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+
+    this.featuredImageRef = React.createRef();
   }
   compopnentConfig() {
     return {
@@ -53,7 +55,12 @@ export default class BlogForm extends Component {
     formData.append("portfolio_blog[title]", this.state.title);
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
     formData.append("portfolio_blog[content]", this.state.content);
-
+    if (this.state.featured_image) {
+      formData.append(
+        "portfolio_blog[featured_image]",
+        this.state.featured_image
+      );
+    }
     return formData;
   }
 
@@ -65,10 +72,15 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then(response => {
+        if (this.state.featured_image) {
+          this.featuredImageRef.current.dropzone.removeAllFiles();
+        }
+
         this.setState({
           title: "",
           blog_status: "",
           content: "",
+          featured_image: "",
         });
 
         this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
@@ -115,6 +127,7 @@ export default class BlogForm extends Component {
 
         <div className="image-uploaders">
           <DropzoneComponent
+            ref={this.featuredImageRef}
             config={this.compopnentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleFeaturedImageDrop()}>

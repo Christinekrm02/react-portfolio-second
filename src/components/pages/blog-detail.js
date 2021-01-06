961fb118b22b510e28711bfbc1c841ae384f430d
Christine Maynard <christinekrm02@gmail.com>
2021-01-06 00:27:38 -0500

    added update functionality. NEXT: enable authorization rules for Blog Detail Component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -16,6 +16,15 @@ export default class BlogDetail extends Component {
     };
     this.handleEditClick = this.handleEditClick.bind(this);
     this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+    this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+      this
+    );
+  }
+  handleUpdateFormSubmission(blog) {
+    this.setState({
+      blogItem: blog,
+      editMode: false,
+    });
   }
   handleFeaturedImageDelete() {
     this.setState({
@@ -59,6 +68,7 @@ export default class BlogDetail extends Component {
         return (
           <BlogForm
             handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+            handleUpdateFormSubmission={this.handleUpdateFormSubmission}
             editMode={this.state.editMode}
             blog={this.state.blogItem}
           />

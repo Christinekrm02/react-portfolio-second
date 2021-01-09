804d520c9e2dab8844289e902adb8d405ade7666
Christine Maynard <christinekrm02@gmail.com>
2021-01-09 16:36:31 -0500

    fixed about import

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
-import About from "./pages/About";
+import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import BlogDetail from "./pages/blog-detail";

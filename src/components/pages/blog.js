5069497908767383f60ad533a8b5c35ec3391b01
Christine Maynard <christinekrm02@gmail.com>
2021-01-09 15:30:29 -0500

    Completed functionality to delete a blog

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -24,6 +24,25 @@ export default class Blog extends Component {
     this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(
       this
     );
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+  handleDeleteClick(blog) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          blogItems: this.state.blogItems.filter(blogItem => {
+            return blog.id !== blogItem.id;
+          }),
+        });
+        return response.data;
+      })
+      .catch(error => {
+        console.log("Delete blog error", error);
+      });
   }
   handleSuccessfulNewBlogSubmission(blog) {
     this.setState({
@@ -87,7 +106,18 @@ export default class Blog extends Component {
   render() {
     //Store blog records
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      if (this.props.loggedInStatus === "LOGGED_IN") {
+        return (
+          <div key={blogItem.id} className="admin-blog-wrapper">
+            <BlogItem blogItem={blogItem} />
+            <a onClick={() => this.handleDeleteClick(blogItem)}>
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
+        );
+      } else {
+        return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      }
     });
 
     return (

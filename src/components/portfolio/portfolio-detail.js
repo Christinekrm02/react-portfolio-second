317e73fcfb0d40e7569a3d5ed8448c49b33db589
Christine Maynard <christinekrm02@gmail.com>
2021-01-09 13:47:23 -0500

    Added api call to get blogitems

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function(props) {
-    return(
-        <div>
-            <h2>Portfolio Detail for {props.match.params.slug}</h2>
-        </div>
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://christinem.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("getPortfolioItem", response);
+      })
+      .catch(error => {
+        console.log("getPortfolioItemError", error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
     );
-    
-};
+  }
+}
